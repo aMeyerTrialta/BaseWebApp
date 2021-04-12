fc0158c52bd3eb1ce86de5e7d60c323f286d7e8e
Andreas Meyer <a.meyer@trialta.de>
2021-04-12 15:27:57 +0200

    Implementation of weather search

--- project/js/main.js
+++ project/js/main.js
@@ -3,11 +3,18 @@ $(document).ready(function() {
 })
 
 function getWeather(searchQuery) {
-  var url="https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=5838ee47a07f52f9fc45c48890d46e10" + apiKey;
+  var url="https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=" + apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
+
   $.ajax(url, {success: function(data){
     console.log(data);
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
+  }, error: function(error) {
+    $(".error-message").text("An error eccoured!");
   }})
 }
 
